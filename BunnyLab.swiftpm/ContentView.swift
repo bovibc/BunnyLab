1db2c76432cee6a19e39b37abb00683bfefff9a4
Clissia Bovi <bovibc@gmail.com>
2024-01-29 11:13:33 -0300

    files

--- BunnyLab.swiftpm/ContentView.swift
+++ BunnyLab.swiftpm/ContentView.swift
@@ -2,10 +2,12 @@ import SwiftUI
 import SpriteKit
 
 struct ContentView: View {
-    @State var currentScene: SKScene = LabScene(size:
-            CGSize(width: 1024, height: 768))
+    let gameScene = SKScene(fileNamed: "LabScene2")
 
     var body: some View {
-        SpriteView(scene: currentScene)
+        if let gameScene {
+            SpriteView(scene: gameScene)
+                .ignoresSafeArea()
+        }
     }
 }
