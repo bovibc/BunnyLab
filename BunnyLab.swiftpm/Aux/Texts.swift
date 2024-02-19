8f89b7ff585f72055ee68a82f67f70a1527a6944
Clissia Bovi <bovibc@gmail.com>
2024-02-19 20:03:09 -0300

    text

--- BunnyLab.swiftpm/Aux/Texts.swift
+++ BunnyLab.swiftpm/Aux/Texts.swift
@@ -7,14 +7,63 @@
 
 import Foundation
 
-
 struct Texts {
-    let cityTexts: [String] = ["aaaaaaaaaaaaaasnsjnjsnsjnjdnjsndjndjnsdjndsjndjdnjsdnjdndjndjndsjnsdjdnjdnjdsjdsndjndjndjns", "bbbbbbbbbbbb", "ccccccccccccc"]
-    let woodsTexts1: [String] = ["dddddddddedmkm"]
-    let woodsTexts2: [String] = ["ccccccc"]
-    let woodsTexts3: [String] = ["bbbbbbb", "sdjdjsk", "dhsihdihddhihdsdih"]
-    let firstExperimentTexts: [String] = ["dnsdskjnskdjnsdjkd", "berijiejejoire", "uuuuuuuu"]
-    let secondExperimentTexts: [String] = ["gugugugugugugug", "ahahahahahaha", "iiiiiiiiiii"]
-    let thirdExperimentTexts: [String] = ["a", "b", "c"]
-    let endTexts: [String] = ["d", "e", "f"]
+    let cityTexts: [String] = [
+        "This is Janet, a girl who has just entered high school.",
+        "Today, Janet had her first class on genetics and became interested in the subject, but she didn't understand some concepts.",
+        "/\"Genes? Alleles? Recessive and dominant genes? How do I learn all of this?\" Janet wonders.",
+        "To reflect on the subject, Janet decided to walk through the woods near her house."
+    ]
+
+    let woodsTexts1: [String] = [
+        "Why are some baby birds in this nest born with feathers of the father's colors and others of the mother's?"
+    ]
+
+    let woodsTexts2: [String] = [
+        "Why do these trees of the same species have foliage of various colors?"
+    ]
+
+    let woodsTexts3: [String] = [
+        "Suddenly Janet comes across something strange....",
+        "Four rabbits of different colors standing in a circle...",
+        "How can there be so many colors?",
+        "...",
+        "They stare at her and suddenly everything goes dark..."
+    ]
+
+    let firstExperimentTexts: [String] = [
+        "Time to start the first experiment!",
+        "Here you'll get some basic concepts about rabbit color genetics!"
+    ]
+
+    let secondExperimentTexts: [String] = [
+        "Here we will learn in practice about dominant and recessive alleles!",
+        "As seen in experiment 1, the color of rabbits is determined by the combination of two alleles (S, C, H, and A).",
+        "The dominant allele is the one that manifests and prevails its characteristic even in heterozygosity (more than one type of allele in the gene).",
+        "The recessive allele is the one that only manifests in homozygosity (only one type of allele in the gene).",
+        "The dominance relationship between the fur alleles of the rabbit are S>C>H>A."
+    ]
+
+    let thirdExperimentTexts: [String] = [
+        "In this experiment, we will learn about genotype and phenotype.",
+        "Genotype is the genetic composition of the individual, in the case of rabbit color, the genotype is the set of alleles.",
+        "Phenotype is the expression of the genotype, meaning, which characteristic is externalized by the individual. In our case, the phenotype is the color of the rabbits."
+    ]
+
+    let endTexts: [String] = [
+        "Now you already know some basic concepts of genetics! Move forward and head home! Have a safe journey!"
+    ]
+
+    let info1 = [
+        "Click on the containers with the colors shown on the board to unlock some concepts about genetics (the order doesn't matter).\nWhen you're done selecting the colors, press the Done button.\nRemember, this is the first of three experiments!"
+    ]
+
+    let info2 = [
+        "Practice the concepts of dominant and recessive genes!\nSelect the alleles and press Merge to visualize the resulting rabbit.\nRemember: A>C>H>W.\nAllele A has dominance over C, H, and W. C has dominance over H and W, and H has dominance over W.\nHeterozygosity is when we have more than one type of allele within the gene. E.g., SC.\nHomozygosity is when we have only one type of allele within the gene. E.g., SS.\nRemember, this is the second of three experiments!"
+    ]
+
+    let info3 = [
+        "Exercise the concepts of genotype and phenotype!\nSelect the parents' genotype to obtain the possible genotypes (allele combinations) and phenotypes (expressed characteristic) of the offspring.\nThis is the last experiment!\nGenotype is the genetic composition of the individual, in the case of rabbit color, the genotype is the set of alleles.\nPhenotype is the expression of the genotype, meaning, which characteristic is externalized by the individual. In our case, the phenotype is the color of the rabbits."
+    ]
+
 }
