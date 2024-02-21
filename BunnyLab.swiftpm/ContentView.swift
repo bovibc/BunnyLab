f3c9145f15455164b39c1b437c7727b1ec705b32
Clissia Bovi <bovibc@gmail.com>
2024-02-21 18:31:16 -0300

    finish

--- BunnyLab.swiftpm/ContentView.swift
+++ BunnyLab.swiftpm/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import SpriteKit
 
 struct ContentView: View {
-    let gameScene = SKScene(fileNamed: Scenes.Lab.rawValue)
+    let gameScene = SKScene(fileNamed: Scenes.Start.rawValue)
 
     var body: some View {
         if let gameScene {
